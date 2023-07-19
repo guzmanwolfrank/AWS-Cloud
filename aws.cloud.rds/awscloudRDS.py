62d3b19571b160946e3a922e26e09c113e3e7b12
Wolf <guzmanwolfrank@gmail.com>
2023-07-19 19:04:42 -0400

    Create awscloudRDS.py
    Here is a Python script for AWS CLI that helps initiate an RDS DB, connect it to an instance, then place a website into the instance and a CSV file to the RDS database.

---
+++ aws.cloud.rds/awscloudRDS.py
@@ -0,0 +1,105 @@
+import boto3
+
+# Get the AWS CLI configuration
+config = boto3.config.Config(region_name="us-east-1")
+
+# Create an RDS client
+rds = boto3.client("rds", config=config)
+
+# Create a database instance
+db_instance_name = "my-db-instance"
+db_instance_class = "db.t2.micro"
+db_engine = "mysql"
+db_name = "my-database"
+db_username = "my-username"
+db_password = "my-password"
+rds.create_db_instance(
+    DBInstanceIdentifier=db_instance_name,
+    DBInstanceClass=db_instance_class,
+    DBEngine=db_engine,
+    DBName=db_name,
+    DBUsername=db_username,
+    DBPassword=db_password,
+)
+
+# Wait for the database instance to be available
+while rds.describe_db_instances()["DBInstances"][0]["DBInstanceStatus"] != "available":
+    time.sleep(5)
+
+# Get the database instance endpoint
+db_instance_endpoint = rds.describe_db_instances()["DBInstances"][0]["Endpoint"]["Address"]
+
+# Create an EC2 client
+ec2 = boto3.client("ec2", config=config)
+
+# Get the VPC ID and subnet ID
+vpc_id = ec2.describe_vpcs()["Vpcs"][0]["VpcId"]
+subnet_id = ec2.describe_subnets(Filters=[{"Name": "Name", "Values": ["default"]}])[
+    "Subnets"
+][0]["SubnetId"]
+
+# Create a security group
+security_group_name = "my-security-group"
+security_group_description = "A security group for my website"
+ec2.create_security_group(GroupName=security_group_name, Description=security_group_description)
+
+# Open ports 80 and 443 in the security group
+ec2.authorize_security_group_ingress(
+    GroupId=security_group_name,
+    IpPermissions=[
+        {
+            "IpProtocol": "tcp",
+            "FromPort": 80,
+            "ToPort": 80,
+            "IpRanges": [{"CidrIp": "0.0.0.0/0"}],
+        },
+        {
+            "IpProtocol": "tcp",
+            "FromPort": 443,
+            "ToPort": 443,
+            "IpRanges": [{"CidrIp": "0.0.0.0/0"}],
+        },
+    ],
+)
+
+# Create an SSH key pair
+key_pair_name = "my-key-pair"
+ec2.create_key_pair(KeyName=key_pair_name)
+
+# Upload the website files
+website_files = ["index.html", "style.css"]
+with open("/tmp/website.zip", "w") as zipfile:
+    for file in website_files:
+        zipfile.write(file)
+
+# Create an EC2 instance
+instance_type = "t2.micro"
+instance_count = 1
+ec2.run_instances(
+    ImageId=ami_id,
+    MinCount=instance_count,
+    MaxCount=instance_count,
+    InstanceType=instance_type,
+    KeyName=key_pair_name,
+    VpcSubnetIds=[subnet_id],
+    SecurityGroupIds=[security_group_name],
+)
+
+# Get the instance ID
+instance_id = ec2.describe_instances()["Reservations"][0]["Instances"][0]["InstanceId"]
+
+# Upload the website to the instance
+ec2.upload_file(
+    Filename="/tmp/website.zip",
+    LocalPath=None,
+    Dest="/home/ec2-user/website.zip",
+    InstanceId=instance_id,
+)
+
+# Unzip the website files on the instance
+ec2.run_command(
+    Command=["unzip", "-q", "/home/ec2-user/website.zip"], InstanceId=instance_id
+)
+
+# Open a browser to the website
+url = "http://" + ec2.describe_instances()["Reservations"
